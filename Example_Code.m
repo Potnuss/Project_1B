8e59c605c6ade0ddd16a0c01f4f7266b25612e62
Raman Haddad <raman.haddad@gmail.com>
2014-11-24 17:48:27 +0100

    Added demodulation, decimation and downsampling

--- Example_Code.m
+++ Example_Code.m
@@ -53,3 +53,19 @@ yib = yrec.*exp(-i*2*pi*fc/fs*n);
 semilogy(F,abs([fft(y,NN) fft(yib,NN) ]) ) % Check transforms
 legend('Modulated','Demodulated')
 xlabel('Frequency (Hz)');
+
+%% Design a LP decimation filter
+B = firpm(32,2*[0 0.5/R*0.9 0.5/R*1.6 1/2],[1 1 0 0]);
+yi = conv(yib,B);
+% Y is now the interpolated signal
+figure(2)
+semilogy(f,abs([fft(yib,NN) fft(yi,NN) fft(B.',NN)]) ) % Check transforms
+legend('Demodulated','after LP filtering','LP-filter')
+xlabel('relative frequency f/fs');
+
+%% Down-sampling
+D = 5;
+y = yi(1:D:end);
+semilogy(f,abs([fft(y,NN)]) ) % Check transforms
+%% Check in time domain
+plot(real(y))
